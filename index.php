ffbf6c11ea57e682590f975e021f231286070dc3
Сенина Юлия Александровна <y.senina@ens.ru>
2023-03-10 11:49:54 +0300

    commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+$variable = 3.14;
+// $variable = 3;
+// $variable = 'one';
+// $variable = true;
+// $variable = null;
+// $variable = [];
+
+echo 'Первая задача: ';
+if (is_bool($variable)) {
+  echo 'type is bool' . "\n";
+} elseif (is_float($variable)) {
+  echo 'type is float' . "\n";
+} elseif (is_int($variable)) {
+  echo 'type is int' . "\n";
+} elseif (is_string($variable)) {
+  echo 'type is string' . "\n";
+} elseif (is_null($variable)) {
+  echo 'type is null' . "\n";
+} else {
+  echo 'type is other' . "\n";
+}
+
+// Доп. задача
+
+$variableNew = 3.14;
+echo 'Вторая задача: ';
+
+switch (gettype($variableNew)) {
+  case 'boolean':
+    echo 'type is bool';
+    break;
+  case 'string':
+    echo 'type is string';
+    break;
+  case 'integer':
+    echo 'type is int';
+    break;
+  case 'double':
+    echo 'type is float';
+    break;
+  case 'NULL':
+    echo 'type is null';
+    break;
+  default:
+    echo 'type is other';
+}
